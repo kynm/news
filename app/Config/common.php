fb5e6f658787bab6b2d736d13b26ef69a071f13f
unknown <ngainguyendv@gmail.com>
2014-12-04 21:58:08 -0800

    add view post

---
+++ app/Config/common.php
@@ -0,0 +1,21 @@
+<?php
+$config = array(
+	'groups' => array(
+		'group1' => array(
+			'category1' => 'category1',
+			'category2' => 'category2',
+			'category3' => 'category3',
+		),
+		'group2' => array(
+			'category1' => 'category1',
+			'category2' => 'category2',
+			'category3' => 'category3',
+		),
+		'group3' => array(
+			'category1' => 'category1',
+			'category2' => 'category2',
+			'category3' => 'category3',
+		),
+	),
+);
+?>
